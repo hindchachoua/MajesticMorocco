2e77a9ee404ea7b53360ad5f3a50a9bd33e734fc
hindchachoua <hindchachoua33@gmail.com>
2024-04-28 23:55:30 +0100

    commit

--- resources/views/admin/order/cancel.blade.php
+++ resources/views/admin/order/cancel.blade.php
@@ -1,14 +1,14 @@
 @extends('layout.layout3')
 @section('content')
 
-<div>
+<div  style="background:linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url({{asset('storage/images/produits-locals.jpg')}})">
     <div class="row">
     <!-- Sidebar -->
     <div class="col-md-3">
     @include('include.sidebar')
     
     </div>
-    <div class="col-md-9" style="background:linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url({{asset('storage/images/produits-locals.jpg')}})">
+    <div class="col-md-9">
         {{-- <div class="container-fluid page-header py-3 wow fadeIn" data-wow-delay="0.1s" style="background:linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url({{asset('storage/images/produits-locals.jpg')}})">
             <div class="container text-center pt-5 pb-3">
                 <h1 class="display-4 text-white animated slideInDown mb-3">Cancel Order</h1>
